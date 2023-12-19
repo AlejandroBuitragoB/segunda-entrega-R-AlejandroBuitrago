489e56d91ff760e96d61829c6e37724c5dc5ab1c
Alejandro Buitrago <alejandrobuitragob1@gmail.com>
2023-12-19 10:03:02 -0400

    arreglo en enlaces

--- src/asyncmock.js
+++ src/asyncmock.js
@@ -1,12 +1,12 @@
 const productos = [
-    { id: "1", nombre: "Proteina grande", precio: 50000, img: "..img/proteina-grande.webp", idCat: "2" },
-    { id: "2", nombre: "Proteina pequeña", precio: 38000, img: "..img/proteina-pequeña.avif", idCat: "2" },
-    { id: "3", nombre: "Proteina vegana", precio: 60000, img: "..img/proteina-vegana.jpg", idCat: "2" },
-    { id: "4", nombre: "Creatina grande", precio: 29000, img: "..img/creatina-grande.webp", idCat: "3" },
-    { id: "5", nombre: "Creatina pequeña", precio: 19000, img: "..img/creatina-pequeña.jpg", idCat: "3" },
-    { id: "6", nombre: "Barras de chocolate", precio: 15000, img: "..img/barritas-chocolate.webp", idCat: "4" },
-    { id: "7", nombre: "Barras de frutos rojos", precio: 15000, img: "..img/barritas-frutos-rojos.jpg", idCat: "4" },
-    { id: "8", nombre: "Barras de maní", precio: 15000, img: "..img/barritas-mani.webp", idCat: "4" },
+    { id: "1", nombre: "Proteina grande", precio: 50000, img: "..public/img/proteina-grande.webp", idCat: "2" },
+    { id: "2", nombre: "Proteina pequeña", precio: 38000, img: "..public/img/proteina-pequeña.avif", idCat: "2" },
+    { id: "3", nombre: "Proteina vegana", precio: 60000, img: "..public/img/proteina-vegana.jpg", idCat: "2" },
+    { id: "4", nombre: "Creatina grande", precio: 29000, img: "..public/img/creatina-grande.webp", idCat: "3" },
+    { id: "5", nombre: "Creatina pequeña", precio: 19000, img: "..public/img/creatina-pequeña.jpg", idCat: "3" },
+    { id: "6", nombre: "Barras de chocolate", precio: 15000, img: "..public/img/barritas-chocolate.webp", idCat: "4" },
+    { id: "7", nombre: "Barras de frutos rojos", precio: 15000, img: "..public/img/barritas-frutos-rojos.jpg", idCat: "4" },
+    { id: "8", nombre: "Barras de maní", precio: 15000, img: "..public/img/barritas-mani.webp", idCat: "4" },
 ]
 
 export const getProductos = () => {
